6c3b05f8f2a06db31b992d9d78453effb29e6925
Bert Spaan <bert.spaan@gmail.com>
2019-05-09 16:20:37 +0200

    wortelen!

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,8 @@
 
 <script>
 
+import { lineChunk, pointToLineDistance } from '@turf/turf'
+
 export default {
   name: 'app',
   components: {
@@ -17,7 +19,52 @@ export default {
   },
   data () {
     return {
-      map: undefined
+      map: undefined,
+      loaded: false,
+      animating: false,
+      glAdded: false,
+      currentDistance: 0,
+      maxDistance: 1000
+    }
+  },
+  methods: {
+    getLineWidth: function (maxDistance) {
+      const maxWidth = 15
+      return ['interpolate',
+        ['linear'],
+        ['get', 'distance'],
+        0, maxWidth,
+        Math.max(maxDistance - 50, 1), maxWidth,
+        Math.max(maxDistance, 2), 0
+      ]
+    },
+    reset: function () {
+      this.currentDistance = 0
+      this.updateLayerStyle()
+    },
+    updateLayerStyle: function () {
+      const lineWidth = this.getLineWidth(this.currentDistance)
+      this.map.setPaintProperty('chunks', 'line-width', lineWidth)
+    },
+    startAnimating: function () {
+      this.reset()
+      this.animating = true
+
+      const vm = this
+
+      function animate () {
+        vm.currentDistance += 5
+        vm.updateLayerStyle()
+
+        if (vm.animating && vm.currentDistance < vm.maxDistance) {
+          requestAnimationFrame(animate)
+        }
+      }
+
+      animate()
+    },
+    stopAnimating: function () {
+      this.animating = false
     }
   },
   mounted: function () {
@@ -27,11 +74,125 @@ export default {
       style: 'https://ta.webmapper.nl/wm/styles/topography.json',
       center: [4.922, 52.369],
       zoom: 9,
+      minZoom: 15,
       hash: true
     })
 
     map.on('load', () => {
+      this.loaded = true
 
+      const chunks = {
+        type: 'FeatureCollection',
+        features: []
+      }
+
+      map.addSource('chunks', {
+        type: 'geojson',
+        data: chunks
+      })
+
+      map.addLayer({
+        id: 'chunks',
+        type: 'line',
+        source: 'chunks',
+        paint: {
+          'line-color': '#022520',
+          'line-opacity': 0.8,
+          'line-width': ['interpolate',
+            ['linear'],
+            ['get', 'distance'],
+            0, 6,
+            this.maxDistance, 0
+          ]
+        }
+      })
+    })
+
+    map.on('mousedown', (event) => {
+      if (!this.loaded) {
+        return
+      }
+
+      const point = {
+        type: 'Point',
+        coordinates: [event.lngLat.lng, event.lngLat.lat]
+      }
+
+      const features = map.querySourceFeatures('wm_visdata', {
+        sourceLayer: ['roads']
+      })
+
+      const chunks = []
+      let id = 0
+
+      features.forEach((feature) => {
+        const featureChunks = lineChunk(feature.geometry, 25, {units: 'meters'})
+        featureChunks.features.forEach((feature) => {
+          chunks.push({
+            type: 'Feature',
+            id: id++,
+            properties: {
+              distance: pointToLineDistance(point, feature, {units: 'meters'})
+            },
+            geometry: feature.geometry
+          })
+        })
+      })
+
+      const geojson = {
+        type: 'FeatureCollection',
+        features: chunks
+      }
+
+
+      if (!this.glAdded) {
+        map.addLayer({
+          id: 'custom',
+          type: 'custom',
+          renderingMode: '3d',
+
+          onAdd: function (map, mbxContext) {
+            this.threebox = new Threebox(
+              map,
+              mbxContext,
+              {defaultLights: true}
+            )
+
+            chunks.forEach((chunk, chunkIndex) => {
+              // chunk.properties.distance
+              const lineOptions = {
+                geometry: chunk.geometry.coordinates.map((coordinate, index) =>
+                  [
+                    ...coordinate,
+                    index === 0 ? (chunkIndex === 0 ? 0 : chunks[chunkIndex - 1].properties.distance / 5) : chunk.properties.distance / 5
+                  ]
+                ),
+              	color: 0x33ee22 * chunk.properties.distance,
+              	width: 6
+              }
+
+              const lineMesh = this.threebox.line(lineOptions)
+              this.threebox.add(lineMesh)
+            })
+          },
+          render: function (gl, matrix) {
+            this.threebox.update()
+          }
+        })
+      }
+
+
+      this.geojson = geojson
+      map.getSource('chunks').setData(geojson)
+      this.startAnimating()
+    })
+
+    map.on('mouseup', () => {
+      if (!this.loaded) {
+        return
+      }
+
+      this.stopAnimating()
     })
 
     this.map = map
